22e9c71e18187447caf36a088c29ecfb60f7118e
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-06 00:09:30 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,41 +3,50 @@ library(shiny)
 # Training set
 train <- read.csv("training.csv", header = TRUE)
 
-pageWithSidebar(
+fluidPage(theme = shinytheme("united"),
+                
+                # Page header
+                headerPanel("lymphocyte prediction/ Laboratoire d'immunologie"),
+                
+                # Input values
+                sidebarPanel(
+                  HTML("<h3>Input parameters</h3>"),
+                  selectInput("SEXE", label = "SEXE:", 
+                              choices = list("homme" = "1", "femme" = "2"), 
+                              selected = "1"),
+                  sliderInput("AGE", label = "age :",
+                              min = 0, max = 100,
+                              value = 0),
+                  selectInput("STADE", label = "STADE:", 
+                              choices = list("1" = "1", "2" = "2", "3"="3"), 
+                              selected = "0"),
+                  sliderInput("hb", label = "hemoglobine:",
+                              min = 0, max = 18,
+                              value = 0),
+                  sliderInput("vgm", label = "volume globulaire moyen:",
+                              min = 0, max = 120,
+                              value = 0),
+                  sliderInput("gr",label =  "globule rouge:",
+                              min = 0, max = 5,
+                              value = 0),
+                  sliderInput("plq", label = "plaquette:",
+                              min = 0, max = 500,
+                              value = 0),
+                  sliderInput("ht", "hematocrite:",
+                              min = 0, max = 80,
+                              value = 0),
+                  selectInput("tlc12", label = "lymphocyte:", 
+                              choices = list("<1200" = "0", ">=1200" = "1"), 
+                              selected = "0"),
                   
-                  # Page header
-                  headerPanel("lymphocyte prediction/ Laboratoire d'immunologie"),
+                  actionButton("submitbutton", "Submit", class = "btn btn-primary")
+                ),
+                
+                mainPanel(
+                  tags$label(h3('Status/Output')), # Status/Output Text Box
+                  verbatimTextOutput('contents'),
+                  tableOutput('table') # Prediction results table
                   
-                  # Input values
-                  sidebarPanel(
-                    HTML("<h3>Input parameters</h3>"),
-                    
-                    selectInput("STADE", label = "STADE:", 
-                                choices = list("1" = "1", "2" = "2", "3"="3"), 
-                                selected = "1"),
-                    sliderInput("hb", "hemoglobine:",
-                                min = 0, max = 18,
-                                value = 0),
-                    sliderInput("vgm", "volume globulaire moyen:",
-                                min = 0, max = 120,
-                                value = 0),
-                    sliderInput("plq", "plaquette:",
-                                min = 0, max = 400,
-                                value = 0),
-                    sliderInput("ht", "hematocrite:",
-                                min = 0, max = 80,
-                                value = 0),
-                    selectInput("tlc12", label = "lymphocyte:", 
-                                choices = list("<1200" = "<1200", ">1200" = ">1200"), 
-                                selected = "0"),
-                    
-                    actionButton("submitbutton", "Submit", class = "btn btn-primary")
-                  ),
-                  
-                  mainPanel(
-                    tags$label(h3('Status/Output')), # Status/Output Text Box
-                    verbatimTextOutput('contents'),
-                    tableOutput('table') # Prediction results table
-                    
-                  )
-  )
+                )
+)
+
