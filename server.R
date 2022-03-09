6efecafe369d55e6a61ce228937edaa8ea07b765
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-09 23:40:10 +0100

    Update server.R

--- server.R
+++ server.R
@@ -45,6 +45,8 @@ shinyServer(function(input, output, session) {
     write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
     test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
     test <- rbind(over[1,], test)
+    test <- test[-1,]
+
      
     Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
     print(Output)
