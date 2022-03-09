38ddebc5f1bffeead7e4e4b00a61b42e57ab3165
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-09 22:29:35 +0100

    Update server.R

--- server.R
+++ server.R
@@ -6,9 +6,11 @@ library(RCurl)
 
 over <- read.csv("over.csv", header = TRUE,check.names = FALSE)
 # Read in the RF model
+library(randomForest)
 
 model <- readRDS("model.rds")
 
+library(randomForest)
 
 
 shinyServer(function(input, output, session) {  
