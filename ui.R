ebf31aa87276cc5fdc7f7bf2a860395fc6c33414
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-28 22:36:13 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,49 +10,49 @@ library(DT)
 # Training set
 over <- read.csv("over.csv", header = TRUE)
 
-         fluidPage(                
+ ui <- fluidPage(
+                
                 # Page header
-                headerPanel("Prédiction du nombre de CD4/ Laboratoire d'immunologie"),
+                headerPanel("lymphocyte prediction/ Laboratoire d'immunologie"),
                 
                 # Input values
                 sidebarPanel(
-                  HTML("<h2>Paramètres d'entrée</h2>"),
-                  selectInput("SEXE", label = "GENRE:", 
-                              choices = list("Homme" = "1", "Femme" = "2"), 
+                  HTML("<h3>Input parameters</h3>"),
+                  selectInput("SEXE", label = "SEXE:", 
+                              choices = list("homme" = "1", "femme" = "2"), 
                               selected = "1"),
-                  sliderInput("AGE", label = "AGE : année",
-                              min = 18, max = 100,
-                              value = 18),
-                  selectInput("STADE", label = "STADE CDC du VIH:", 
+                  sliderInput("AGE", label = "age :",
+                              min = 0, max = 100,
+                              value = 0),
+                  selectInput("STADE", label = "STADE:", 
                               choices = list("1" = "1", "2" = "2", "3"="3"), 
-                              selected = "1"),
-                  sliderInput("hb", label = "HEMOGLOBINE: g/dl",
-                              min = 5, max = 18,
-                              value = 5),
-                  sliderInput("vgm", label = "VOLUME GLOBULAIRE SANGUIN: µ3 ",
-                              min = 50, max = 120,
-                              value = 50),
-                  sliderInput("gr",label =  "GLOBULE ROUGE: millions par microlitre",
-                              min = 1, max = 5,
-                              value = 1),
-                  sliderInput("plq", label = "PLAQUETTE: *10^3/mm3",
-                              min = 10, max = 500,
-                              value = 10),
-                  sliderInput("ht", "HEMATOCRITE: pourcentage",
-                              min = 30, max = 60,
-                              value = 30),
-                  selectInput("tlc11", label = "NOMBRE DE LYMPHOCYTES: /mm3", 
-                              choices = list("<1100" = "0", ">=1100" = "1"), 
+                              selected = "0"),
+                  sliderInput("hb", label = "hemoglobine:",
+                              min = 0, max = 18,
+                              value = 0),
+                  sliderInput("vgm", label = "volume globulaire moyen:",
+                              min = 0, max = 120,
+                              value = 0),
+                  sliderInput("gr",label =  "globule rouge:",
+                              min = 0, max = 5,
+                              value = 0),
+                  sliderInput("plq", label = "plaquette:",
+                              min = 0, max = 500,
+                              value = 0),
+                  sliderInput("ht", "hematocrite:",
+                              min = 0, max = 80,
+                              value = 0),
+                  selectInput("tlc11", label = "lymphocyte:", 
+                              choices = list("<1200" = "0", ">=1200" = "1"), 
                               selected = "0"),
                   
-                  actionButton("submitbutton", "soumettre", class = "btn btn-primary")
-),
-
-  
-mainPanel(
-          tags$label(h3('État/résultat')), # Status/Output Text Box
-          verbatimTextOutput('contents'),
-          tableOutput('tabledata') # Prediction results table
+                  actionButton("submitbutton", "Submit", class = "btn btn-primary")
+                ),
+                
+                mainPanel(
+                  tags$label(h3('Status/Output')), # Status/Output Text Box
+                  verbatimTextOutput('contents'),
+                  tableOutput('table') # Prediction results table
                   
-  )
+                )
 )
