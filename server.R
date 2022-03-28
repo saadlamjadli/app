601eb666039ff1938680ebecb435483c3f56996b
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-28 22:37:07 +0200

    Update server.R

--- server.R
+++ server.R
@@ -11,10 +11,11 @@ library(randomForest)
 
 model <- readRDS("model.rds")
 
-shinyServer(function(input, output, session) {
+server <- function(input, output, session) {
+  
   # Input 
-   datasetInput <- reactive({    
-     table <- "responses"
+  setInput <- reactive({  
+    
     # outlook,temperature,humidity,windy,play
     df <- data.frame(
       Name = c("SEXE",
@@ -41,14 +42,14 @@ shinyServer(function(input, output, session) {
     df <- rbind(df, cd4count)
     input <- transpose(df)
     write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    
     test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
     test <- rbind(over[1,], test)
     test <- test[-1,]
-    test$SEXE <- factor(test$SEXE, levels=c("1","2"))
-    test$STADE <- factor(test$STADE, levels = c("1","2","3"))
-    test$tlc11 <- factor(test$tlc11, levels=c("0","1"))
-    test$cd4count <- factor(test$cd4count, levels = c("0","1"))  
-     
+    
+
+    
+    
     Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
     print(Output)
     
@@ -63,7 +64,6 @@ shinyServer(function(input, output, session) {
     }
   })
   
-
   # Prediction results table
   output$table <- renderTable({
     if (input$submitbutton>0) { 
@@ -71,5 +71,5 @@ shinyServer(function(input, output, session) {
     } 
   })
   
-})
+}
 
