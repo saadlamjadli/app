da667de3e14196a6b84d0cdf4d64deb61ed4bcb3
saadlamjadli <96699229+saadlamjadli@users.noreply.github.com>
2022-03-29 10:29:01 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,47 +11,47 @@ over <- read.csv("over.csv", header = TRUE)
 
 fluidPage(
   # Page header
-  headerPanel("prediction du nombre de CD4/ Laboratoire d'immunologie"),
+  headerPanel("Prediction du nombre de CD4/ Laboratoire d'immunologie"),
   
   # Input values
   sidebarPanel(
-    HTML("<h3>Input parameters</h3>"),
+    HTML("<h2>Paramètres d'entrée</h2>"),
     
-    selectInput("SEXE", label = "SEXE:", 
+    selectInput("SEXE", label = "Genre:", 
                 choices = list("homme" = "1", "femme" = "2"), 
                 selected = "1"),
-    sliderInput("AGE", label = "age :",
-                min = 0, max = 100,
-                value = 0),
-    selectInput("STADE", label = "STADE:", 
+    sliderInput("AGE", label = "Age : / an",
+                min = 18, max = 100,
+                value = 18),
+    selectInput("STADE", label = "STADE CDC du VIH :", 
                 choices = list("1" = "1", "2" = "2", "3"="3"), 
-                selected = "0"),
-    sliderInput("hb", label = "hemoglobine:",
-                min = 0, max = 18,
-                value = 0),
-    sliderInput("vgm", label = "volume globulaire moyen:",
-                min = 0, max = 120,
-                value = 0),
-    sliderInput("gr",label =  "globule rouge:",
-                min = 0, max = 5,
-                value = 0),
-    sliderInput("plq", label = "plaquette:",
-                min = 0, max = 500,
-                value = 0),
-    sliderInput("ht", "hematocrite:",
-                min = 0, max = 80,
-                value = 0),
-    selectInput("tlc11", label = "lymphocyte:", 
+                selected = "1"),
+    sliderInput("hb", label = "Taux d'hémoglobine : g/dL",
+                min = 4, max = 18,
+                value = 4),
+    sliderInput("vgm", label = "volume globulaire moyen: µm3",
+                min = 70, max = 120,
+                value = 70),
+    sliderInput("gr",label =  "Nombre de globules rouge: millions par mm3",
+                min = 2, max = 6,
+                value = 2),
+    sliderInput("plq", label = "Nombre de plaquette:*103 /mm³",
+                min = 10, max = 500,
+                value = 10),
+    sliderInput("ht", "Taux d'hématocrite: en pourcentage",
+                min = 30, max = 60,
+                value = 30),
+    selectInput("tlc11", label = "Nombre de lymphocyte: /mm3 ", 
                 choices = list("<1100" = "0", ">=1100" = "1"), 
                 selected = "0"),
     
     
     
-    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+    actionButton("submitbutton", "Soumettre", class = "btn btn-primary")
   ),
   
   mainPanel(
-    tags$label(h3('Status/Output')), # Status/Output Text Box
+    tags$label(h3('État/Résultat')), # Status/Output Text Box
     verbatimTextOutput('contents'),
     tableOutput('table')
     
